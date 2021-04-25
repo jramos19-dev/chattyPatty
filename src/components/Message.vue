9f528601484b8d07aea04d5ad3d1cccb2c5a61d9
jramos19-dev <jramos19@southtexascollege.edu>
2021-04-25 12:31:27 -0500

    Working App No animations

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,15 +1,44 @@
 <template>
-  <div class="container mx-auto flex my-3 rounded-full">
+  <div
+    class="container mx-auto flex my-3 rounded-full"
+    :class="isUser ? 'bg-amber-300' : 'bg-green-500'"
+  >
     <div>
-      <mdi:shield-account class="bg-gray-500 h-12 w-12 rounded-full" />
+      <img
+        class="rounded-full h-12"
+        :src="message.userPhotoURL"
+        :alt="`Avatar of ${message.userName}`"
+      />
     </div>
     <div class="flex flex-grow text-xl bg-gray-500 rounded-xl">
       <div
-        class="flex flex-grow items-center justify-between px-8 rounded-full"
+        class="flex flex-grow items-center text-warmGray-200 justify-between px-8 rounded-full"
       >
-        <p>Message Goes Here</p>
-        <p class="text-sm">Author Goes Here</p>
+        <p>{{ message.text }}</p>
+        <p class="text-sm">{{ message.userName }}</p>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { defineProps, computed, ref } from "vue"
+import { authentication } from "~/helpers/useFireBase"
+
+const { user } = authentication()
+
+const isUser = computed(() => user.value.uid === props.message.userId)
+
+const props = defineProps({
+  message: {
+    type: Object,
+    default: () => ({
+      userName: "",
+      userId: "",
+      userPhotoURL: "",
+      text: "",
+      createAt: "",
+    }),
+  },
+})
+</script>
--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -2,9 +2,30 @@
   <div
     class="absolute bottom-0 left-0 right-0 bg-blue-400 flex focus-within:ring-yellow-800"
   >
-    <input type="text" class="flex-grow text-xl font-thin focus:outline-none" />
-    <button class="hover:bg-blue-600 rounded-sm px-8 focus:outline-none">
+    <input
+      v-model="newMessage"
+      type="text"
+      class="flex-grow text-xl font-thin focus:outline-none"
+      @change="send"
+    />
+    <button
+      class="hover:bg-blue-600 rounded-sm px-8 focus:outline-none"
+      @click="send"
+    >
       <mdi:send />
     </button>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue"
+import { database } from "~/helpers/useFireBase"
+const { sendMessage } = database()
+const newMessage = ref(null)
+const send = () => {
+  if (newMessage.value?.length > 0) {
+    sendMessage(newMessage.value)
+    newMessage.value = null
+  }
+}
+</script>
