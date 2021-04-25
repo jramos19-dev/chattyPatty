678539a1d4b06d3d676de9cdd91ea0bf69bb88f0
jramos19-dev <jramos19@southtexascollege.edu>
2021-04-24 19:27:59 -0500

    fixed colors on layout

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container mx-auto flex my-3 rounded">
+  <div class="container mx-auto flex my-3 rounded-full">
     <div>
-      <mdi:shield-account class="bg-gray-500 h-12 w-12 rounded" />
+      <mdi:shield-account class="bg-gray-500 h-12 w-12 rounded-full" />
     </div>
-    <div class="flex flex-grow text-xl bg-green-300">
-      <div class="flex flex-grow items-center justify-between">
+    <div class="flex flex-grow text-xl bg-gray-500 rounded-xl">
+      <div
+        class="flex flex-grow items-center justify-between px-8 rounded-full"
+      >
         <p>Message Goes Here</p>
         <p class="text-sm">Author Goes Here</p>
       </div>
